fef55b713d0fcc489a3533b439f5e0ab0956c233
halth909 <thomas.hall.12@gmail.com>
2022-08-10 21:19:47 +1200

    sfx

---
+++ interface/renderer.js
@@ -0,0 +1,29 @@
+async function init() {
+    $(document).on('click', '.sfx-button', function() {
+        console.log(this);
+        new Howl({ src: [$(this).attr('url')] }).play()
+    });
+
+    let $sfxDirectory = $('#sfx-directory');
+    let $sfxButtonContainer = $('#sfx-button-container');
+
+    $sfxDirectory.on('change', function(e) {
+        $sfxButtonContainer.empty();
+
+        const files = this.files;
+
+        for (let i = 0; i < files.length; i++) {
+            let file = files.item(i);
+            console.log(file.name);
+            $sfxButtonContainer.append($.parseHTML(`
+                <button class="sfx-button" url='${file.path}'>${file.name}</button>
+            `));
+        }
+    });
+
+    console.log($sfxDirectory);
+}
+
+$(document).ready(() => {
+    init();
+});
